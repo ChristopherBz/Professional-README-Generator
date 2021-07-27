60cd2c52285737740ff2bed0defe9d9bfb7665f9
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-07-28 09:37:51 +1000

    js notes

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
+// description, installation instructions, usage information, contribution guidelines, and test instructions
 const questions = [];
 
 // TODO: Create a function to write README file
+// sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
+// choose a license for my application from a list of options
 function init() {}
 
 // Function call to initialize app
+// badge for that license is added near the top of the README
 init();
