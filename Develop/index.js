443155b489fa9ba3f165f7c55adeddfcbd925b6c
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-08-03 10:09:43 +1000

    readme testing

--- Develop/index.js
+++ Develop/index.js
@@ -62,8 +62,33 @@ function promptUser () {
     },
     {
         type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please describe the usage for your project.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, or supporting resources used',
+    },
+    {
+        type: 'input',
         name: 'contribute',
         message: 'Please state if others can contribute.',
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Please state your projects features.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please state how tests can be completed.',
     },
     {
       type: 'input',
@@ -100,33 +125,30 @@ function generateText(answers) {
   - [Credits](#credits)
   - [License](#license)
   - [Contribution](#contribution)
+  - [Features](#features)
   - [Tests](#tests)
   - [Questions](#questions)
   
   ## Installation
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+  Here are the steps required to get the development environment running:
+  ${answers.installation}
   ## Usage
+  The usage information for this project:
+  ${answers.usage}
   ## Credits
-  List your collaborators, if any, with links to their GitHub profiles.
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  If you followed tutorials, include links to those here as well.
+  ${answers.credits}
   ## License
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ![License](https://img.shields.io/badge/License-${answers.license}-blue.svg "License Badge")
   ## Contribution Guidelines
-  ## Tests
-  ## Questions
-  ---
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  ## Badges
-  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  ${answers.contribution}
   ## Features
-  If your project has a lot of features, list them here.
-  ## How to Contribute
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  ${answers.features}
   ## Tests
-  Go the extra mile and write tests for your application. 
-
+  ${answers.tests}
+  ## Questions
+  ${answers.questions}
+  ---
+ 
 `;
 }
 
